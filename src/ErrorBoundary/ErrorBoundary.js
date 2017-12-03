fd7c60a65d27600165cb4b3e864cca1ad96d2571
kozmobot <kozmobot@gmail.com>
2017-12-03 23:17:19 +0300

    add error handling example

---
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  state = {
+    error: null
+  };
+  componentDidCatch = (error, errorInfo) => {
+    this.setState({ error: error });
+  };
+  render() {
+    if (this.state.error) {
+      return <h1>{this.state.error}</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,6 +2,11 @@ import React from 'react';
 import classes from './Person.css';
 
 const Person = props => {
+  const rand = Math.random();
+  if (rand > 0.7) {
+    throw new Error('I crashed!!!');
+  }
+
   return (
     <div className={classes.person}>
       <p onClick={props.click}>
